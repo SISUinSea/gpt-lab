bc23b26162a90677a55841e381c124a9bf5418fc
SISU <atgsisu@gmail.com>
2026-05-30 22:30:18 +0900

    feat: attention 구현

--- src/attention.py
+++ src/attention.py
@@ -27,11 +27,18 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         if d_model % n_heads != 0:
             raise ValueError("d_model must be divisible by n_heads")
-        self.d_model = d_model
+        self.d_model = d_model  # 이게 책에서는 d_out
         self.n_heads = n_heads
         self.head_dim = d_model // n_heads
-        # TODO: qkv projection, output projection, dropout을 정의하세요.
-        raise NotImplementedError("MultiHeadAttention.__init__을 구현하세요.")
+        
+        self.W_query = nn.Linear(d_model, d_model, qkv_bias)
+        self.W_key = nn.Linear(d_model, d_model, qkv_bias)
+        self.W_value = nn.Linear(d_model, d_model, qkv_bias)
+
+        self.out_proj = nn.Linear(d_model, d_model)  # TODO. 이거 왜 해주는거임??? -> 인정!
+        self.dropout = nn.Dropout(drop_rate)
+
+        
 
     def forward(
         self,
@@ -47,4 +54,48 @@ class MultiHeadAttention(nn.Module):
             causal_mask: True이면 미래 위치를 볼 수 없게 mask 처리
             return_attention_weights: True이면 attention weight도 함께 반환
         """
-        raise NotImplementedError("MultiHeadAttention.forward를 구현하세요.")
+        b, seq_len, _ = x.shape
+        
+        # keys, values, queries 를 만든다.
+        keys = self.W_key(x)
+        values = self.W_value(x)
+        queries = self.W_query(x)
+
+        # kvq를 (batch, tokens, head num , head dim)으로 해석하는 텐서로 만든다.
+        keys = keys.view(b, seq_len, self.n_heads, self.head_dim)
+        values = keys.view(b, seq_len, self.n_heads, self.head_dim)
+        queries = keys.view(b, seq_len, self.n_heads, self.head_dim)
+
+        # kvq를 (batch, head num, tokens, head dim)으로 transpose 한다.
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+
+        # attention scores를 계산한다.
+        attention_scores = queries @ keys.transpose(2, 3)
+
+        # causal mask를 적용한다(causal_mask가 true인 경우에만 적용)
+        if causal_mask:
+            mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1)
+            attention_scores = attention_scores.masked_fill(mask.bool(), -torch.inf)
+
+
+        # softmax를 적용해서 attention weight을 계산한다.
+        attention_weights = torch.softmax(attention_scores / self.head_dim ** 0.5, dim=-1)
+        
+        # dropout을 적용한다.
+        attention_weights = self.dropout(attention_weights)
+
+        # 최종적인 context vector 값을 계산한다.
+        context_vector = (attention_weights @ values).transpose(1, 2)
+
+        # context vector의 head를 결합한다.
+        context_vector = context_vector.contiguous().view(b, seq_len, self.d_model)
+
+        # out_projection을 통과시켜 값들을 합친다.
+        context_vector = self.out_proj(context_vector)
+
+        return context_vector, attention_weights if return_attention_weights else context_vector
+
+        
+
